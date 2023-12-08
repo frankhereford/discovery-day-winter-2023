d3e1c4f329c8587fb0c6e4f769840d1a7811d1a9
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-08 12:57:06 -0600

    Get some real output from the openai api
    Refactor documentarian.js and update OpenAI API usage
    
    Cleaned up the code in documentarian.js by removing commented-out code, unused variables, and whitespace. The OpenAI API call was updated to use a newer model and a more detailed prompt, aiming to improve the quality of code explanations. Added asynchronous processing of nodes for analysis.

--- documentarian.js
+++ documentarian.js
@@ -7,32 +7,19 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const { parse } = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
-//import OpenAI from "openai";
 const OpenAI = require("openai");
 
-
-
-
 const openai = new OpenAI({});
-//const openai = new OpenAI({ });
 
-// Optional: const jscodeshift = require('jscodeshift');
-
-//openai.apiKey = process.env.OPENAI_API_KEY;
-
-
-// Parse the command line arguments
-const argv = yargs(hideBin(process.argv)).option('file', {
-  alias: 'f',
-  describe: 'Path to the JavaScript file',
-  type: 'string',
+const argv = yargs(hideBin(process.argv)).option("file", {
+  alias: "f",
+  describe: "Path to the JavaScript file",
+  type: "string",
   demandOption: true,
 }).argv;
 
-// Retrieve the file path from the argument
 const filePath = argv.file;
 
-// Check if the file exists before reading it
 if (!fs.existsSync(filePath)) {
   console.error(`The file "${filePath}" does not exist.`);
   process.exit(1);
@@ -40,91 +27,94 @@ if (!fs.existsSync(filePath)) {
 
 let fileContent;
 try {
-  // Read the content of the provided file
-  fileContent = fs.readFileSync(filePath, 'utf-8');
+  fileContent = fs.readFileSync(filePath, "utf-8");
 } catch (err) {
   console.error(`An error occurred while reading the file: ${err.message}`);
   process.exit(1);
 }
 
-// Parse the file content to AST
 let ast;
 try {
   ast = parse(fileContent, {
     sourceType: "module",
-    plugins: ["jsx"], // If you are using JSX
-    //errorRecovery: true,
-    //allowAwaitOutsideFunction: true,
-    //allowReturnOutsideFunction: true,
-    //allowUndeclaredExports: true,
+    plugins: ["jsx"],
   });
 } catch (err) {
-  if (err.code === "BABEL_PARSER_SYNTAX_ERROR") {
-    console.error(`Syntax error in file: ${filePath}`);
-    console.error(`Error message: ${err.message}`);
-
-    const lines = fileContent.split("\n");
-    const errorLine = err.loc.line;
-
-    if (lines.length >= errorLine) {
-      console.error(`Problematic line (${errorLine}): ${lines[errorLine - 1]}`);
-    } else {
-      console.error(`Could not locate the problematic line.`);
-    }
-  } else {
-    console.error(err);
-  }
+  console.error(`Parsing error: ${err.message}`);
   process.exit(1);
 }
 
 function printRedDashLine() {
-  const line = '-'.repeat(80);
-  const red = '\x1b[31m'; // ANSI code for red
-  const reset = '\x1b[0m'; // ANSI code to reset color
-
+  const line = "-".repeat(80);
+  const red = "\x1b[31m";
+  const reset = "\x1b[0m";
   console.log(red + line + reset);
 }
 
-async function queryOpenAI(prompt, code) {
+async function queryOpenAI(snippet, code) {
+  prompt = `Take a deep breath and think through everything I ask logically. Check your work.
+I am going to show you a file of javascript. The file will begin after this paragraph,
+and it will continue until I tell you the whole file is done. After a blank line,
+I will include a portion of the same code as before, and I want you to describe what the 
+portion of code does, as if you were writing an inline comment for the file. Be brief
+when you are able, but also be detailed. The whole file follows:
+
+  ` + code + `
+
+The whole file is above, and the portion of the code repeated below is what I want you
+to describe. This portion of the code follows:
+
+  ` + snippet;
+  //console.log(prompt);
+
+  try {
     const params = {
-    messages: [{ role: "user", content: prompt + "\n" + code }],
-    //model: "gpt-4",
-    model: 'gpt-3.5-turbo',
+      messages: [{ role: "user", content: prompt + "\n" + code }],
+      model: "gpt-4-1106-preview",
     };
 
-  
     const chatCompletion = await openai.chat.completions.create(params);
     if (chatCompletion?.choices[0]?.message?.content) {
-    const topicObj = JSON.parse(chatCompletion.choices[0].message.content)
-    return {
-        label: topicObj.label,
-        topic: topicObj.topic,
-    };
+      return chatCompletion.choices[0].message.content;
     } else {
-    return {
-        topic: 'Error: Could not retrieve prompt from OpenAI',
-    };
+      return {
+        topic: "Error: Could not retrieve prompt from OpenAI",
+      };
     }
-
-
+  } catch (err) {
+    console.error(`Error querying OpenAI: ${err.message}`);
+    return {
+      topic: "Error querying OpenAI",
+    };
+  }
 }
 
+const nodesToProcess = [];
 
-const handleNode = async (node) => {
-  printRedDashLine();
-  //console.log(node);
-  const { start, end } = node;
-  const codeSnippet = fileContent.slice(start, end);
-  console.log(codeSnippet, "\n");
-  let response = await queryOpenAI("What does this code do?", codeSnippet);
-  console.log(response)
-};
-
-// Traverse the AST
+// Collect nodes first
 traverse(ast, {
   enter(path) {
-    if (path.isFunctionDeclaration() || path.isArrowFunctionExpression() || path.isClassDeclaration()) {
-      handleNode(path.node);
+    if (
+      path.isFunctionDeclaration() ||
+      path.isArrowFunctionExpression() ||
+      path.isClassDeclaration()
+    ) {
+      nodesToProcess.push(path.node);
     }
-  }
+  },
 });
+
+// Process nodes asynchronously
+async function processNodes() {
+  for (const node of nodesToProcess) {
+    printRedDashLine();
+    const { start, end } = node;
+    const codeSnippet = fileContent.slice(start, end);
+    console.log(codeSnippet, "\n");
+    let response = await queryOpenAI(codeSnippet, fileContent);
+    console.log(response);
+  }
+}
+
+// Run the async function
+processNodes().then(() => console.log("Analysis complete."));
