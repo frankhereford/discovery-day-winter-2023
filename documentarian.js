ef684daaabbc86449f8c8845ad98f8c1195a6b09
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-08 09:51:22 -0600

    start cobbling together the parser
    Enhance script to accept file path as CLI argument
    
    The documentarian.js script now uses the yargs library to parse command line arguments, allowing the user to specify the JavaScript file path with a --file or -f option. The file existence is checked before it is read, improving error handling and user feedback when the specified file does not exist or cannot be read. These changes necessitate users to provide the file path via the CLI, altering previous behavior where the file path was hardcoded.

--- documentarian.js
+++ documentarian.js
@@ -3,17 +3,44 @@
 require("dotenv").config();
 
 const fs = require("fs");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 const { parse } = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 const openai = require("openai");
+
 // Optional: const jscodeshift = require('jscodeshift');
 
 openai.apiKey = process.env.OPENAI_API_KEY;
 
 
-// Read the JavaScript file
-const filePath = './example.js'; // Change this path to your JS file
-const fileContent = fs.readFileSync(filePath, 'utf-8');
+// Parse the command line arguments
+const argv = yargs(hideBin(process.argv)).option('file', {
+  alias: 'f',
+  describe: 'Path to the JavaScript file',
+  type: 'string',
+  demandOption: true, // This option is required
+}).argv;
+
+// Retrieve the file path from the argument
+const filePath = argv.file;
+
+// Check if the file exists before reading it
+if (!fs.existsSync(filePath)) {
+  console.error(`The file "${filePath}" does not exist.`);
+  process.exit(1);
+}
+
+let fileContent;
+try {
+  // Read the content of the provided file
+  fileContent = fs.readFileSync(filePath, 'utf-8');
+} catch (err) {
+  console.error(`An error occurred while reading the file: ${err.message}`);
+  process.exit(1);
+}
+
+console.log(fileContent);
 
 // Parse the file content to AST
 const ast = parse(fileContent, {
@@ -21,11 +48,8 @@ const ast = parse(fileContent, {
   plugins: ['jsx'] // If you are using JSX
 });
 
-// Function to handle each node in AST
-const handleNode = (node) => {
-  // You will fill this in to handle each node, e.g., function, class, etc.
-};
 
+/*
 // Traverse the AST
 traverse(ast, {
   enter(path) {
@@ -34,9 +58,12 @@ traverse(ast, {
     }
   }
 });
+*/
 
+/*
 // Define handleNode function to extract code snippets
 const handleNode = (node) => {
   // Extract code snippet logic goes here
   // For example, get the code of the entire function or class
 };
+*/
