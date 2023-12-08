5fc0f9283e2d697828e2b7a76b11c60ed6abe071
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-08 09:39:46 -0600

    start laying in the basics
    Add initial documentarian script for code analysis
    
    Introduced a new node script 'documentarian.js' that sets up an environment for extracting JavaScript code snippets using @babel/parser and @babel/traverse. The script reads a JS file, converts the content to an Abstract Syntax Tree (AST), then traverses it, looking specifically for functions and classes to handle them accordingly. This setup includes the integration with OpenAI's API, indicating potential future features for automated documentation or code analysis leveraging AI.

---
+++ documentarian.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const { parse } = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const openai = require("openai");
+// Optional: const jscodeshift = require('jscodeshift');
+
+openai.apiKey = process.env.OPENAI_API_KEY;
+
+
+// Read the JavaScript file
+const filePath = './example.js'; // Change this path to your JS file
+const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+// Parse the file content to AST
+const ast = parse(fileContent, {
+  sourceType: 'module',
+  plugins: ['jsx'] // If you are using JSX
+});
+
+// Function to handle each node in AST
+const handleNode = (node) => {
+  // You will fill this in to handle each node, e.g., function, class, etc.
+};
+
+// Traverse the AST
+traverse(ast, {
+  enter(path) {
+    if (path.isFunctionDeclaration() || path.isArrowFunctionExpression() || path.isClassDeclaration()) {
+      handleNode(path.node);
+    }
+  }
+});
+
+// Define handleNode function to extract code snippets
+const handleNode = (node) => {
+  // Extract code snippet logic goes here
+  // For example, get the code of the entire function or class
+};
